18cdbe0d64ab94590946df51cf4705bdf4f9cddd
dmymrin1995 <dmymrin1995@gmail.com>
2024-06-03 12:34:25 +0800

    async database

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 import asyncio
+import asyncpg
 
-import aiohttp
-from aiohttp import ClientSession 
+from typing import List, Tuple, Union
+from random import sample
 
-from util import async_timed
-from chapter_4 import fetch_status
 
-@async_timed()
+def load_common_words():
+    with open("common_words.txt") as common_words:
+        return common_words.readlines()
+
+
+def generate_brand_names(words: List[str]):
+    return [(words[index],) for index in sample(range(100), 100)]
+
+
+async def insert_brands(common_words, connection):
+    brands = generate_brand_names(common_words)
+    insert_brands = "INSERT INTO brand VALUES(DEFAULT, $1)"
+    return await connection.executemany(insert_brands, brands)
+
+
 async def main():
-    async with aiohttp.ClientSession() as session:
-        urls = ['https://example.ru' for _ in range(15)]
-        requests = [fetch_status(session, url, 10) for url in urls]
-        status_codes = await asyncio.gather(*requests)
-        print(status_codes)
+    connection = await asyncpg.connect(
+        host="127.0.0.1",
+        port=5432,
+        user="postgres",
+        database="products",
+        password="admin",
+    )
+    common_words = load_common_words()
+
+    await insert_brands(common_words, connection)
+
 
 asyncio.run(main())
